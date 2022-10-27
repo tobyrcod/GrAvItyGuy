021e9fc579c27c2e164bc52bca6d87047117715d
tobyrcod <61169763+tobyrcod@users.noreply.github.com>
2022-10-27 22:17:48 +0100

    Clamp Player Position to stay on screen
    Weird bug though, the player seems to be being rendered 1 pixel too high, as (0, 0) misses the top pixel of the player and (0, HEIGHT - 1 - player.height) is one pixel off the floor.

--- Code/gameplay/player.py
+++ Code/gameplay/player.py
@@ -4,7 +4,7 @@ from utils import *
 class Player:
 
     speed = 20
-    swap_impulse = -30
+    swap_impulse = -10
 
     def __init__(self, width, height):
         self.size = pygame.Vector2(width, height)
@@ -18,7 +18,16 @@ class Player:
 
     def update(self, delta_time):
         self.velocity.y += GRAVITY * delta_time
-        self.position += self.velocity * delta_time
+
+        new_position = self.position + self.velocity * delta_time
+        if new_position.y < 1:
+            new_position.y = 1
+            self.velocity.y = 0
+        if new_position.y > HEIGHT - self.size.y:
+            new_position.y = HEIGHT - self.size.y
+            self.velocity.y = 0
+
+        self.position = new_position
 
     def get_render_info(self):
         surface = pygame.Surface(self.size)
